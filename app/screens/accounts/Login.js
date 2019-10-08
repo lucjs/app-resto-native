81a6af20eebb25fdb23e97b5d2e73a533a3f6073
developercello <lucaspercello@gmail.com>
2019-10-08 08:30:56 -0300

    Login Image

--- app/screens/accounts/Login.js
+++ app/screens/accounts/Login.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator} from 'react-native';
+import { Image } from 'react-native-elements';
 
 export default class Login extends Component {
     render(){
         return (
-            <View style={styles.viewBody}>
-                <Text>Login Screen...</Text>
-            </View>
+          <View style={styles.viewBody}>
+            <Image
+              source={require('../../../assets/img/5-tenedores-letras-icono-logo.png')}
+              style={{ width: 200, height: 200 }}
+              PlaceholderContent={<ActivityIndicator />}
+              resizeMode="contain"
+            />          
+          </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     viewBody: {
-        flex: 1,       
-        justifyContent: "center",
-        alignItems: "center",
+        flex: 1,      
+        marginTop: 40,
         marginLeft: 40,
-        marginRight: 40       
+        marginRight: 40,
+        alignItems: "center"       
     },
+    logo: {
+        width: 300, 
+        height: 150
+    }
 });
