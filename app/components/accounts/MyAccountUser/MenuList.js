5f05d8bb9ccf964c83dca2feaa69269b119e3491
developercello <lucaspercello@gmail.com>
2019-10-10 21:02:14 -0300

    perfil v.0.0.1

---
+++ app/components/accounts/MyAccountUser/MenuList.js
@@ -0,0 +1,29 @@
+export default (menuList = [
+    {
+      title: "Cambiar Nombre y Apellido",
+      iconType: "material-comunity",
+      iconNameright: "chevron-right",
+      iconColorRight: "#ccc",
+      iconNameLeft: "account-circle",
+      iconColorLeft: "#ccc",
+      onPress: () => {}
+    },
+    {
+        title: "Cambiar Email",
+        iconType: "material-comunity",
+        iconNameright: "chevron-right",
+        iconColorRight: "#ccc",
+        iconNameLeft: "email",
+        iconColorLeft: "#ccc",
+        onPress: () => {}
+      },
+      {
+        title: "Cambiar Contraseña",
+        iconType: "material-comunity",
+        iconNameright: "chevron-right",
+        iconColorRight: "#ccc",
+        iconNameLeft: "lock",
+        iconColorLeft: "#ccc",
+        onPress: () => {}
+      }
+  ]);
--- app/components/accounts/MyAccountUser/UpdateUserInfo.js
+++ app/components/accounts/MyAccountUser/UpdateUserInfo.js
@@ -1,20 +1,73 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { ListItem } from "react-native-elements";
 
 export default class UpdateUserInfo extends Component {
-                 constructor(props) {
-                   super(props);
-                   //console.log(props);
-                 }
+  constructor() {
+    super();
+    this.state = {
+      menuItems: [
+        {
+          title: "Cambiar Nombre y Apellido",
+          iconType: "material-comunity",
+          iconNameRight: "chevron-right",
+          iconColorRight: "#ccc",
+          iconNameLeft: "account-circle",
+          iconColorLeft: "#ccc",
+          onPress: () => {}
+        },
+        {
+          title: "Cambiar Email",
+          iconType: "material-comunity",
+          iconNameRight: "chevron-right",
+          iconColorRight: "#ccc",
+          iconNameLeft: "email",
+          iconColorLeft: "#ccc",
+          onPress: () => {}
+        },
+        {
+          title: "Cambiar Contraseña",
+          iconType: "material-comunity",
+          iconNameRight: "chevron-right",
+          iconColorRight: "#ccc",
+          iconNameLeft: "lock",
+          iconColorLeft: "#ccc",
+          onPress: () => {}
+        }
+      ]
+    };
+  }
 
-                 render() {                  
-                   return (
-                     <View >                      
-                       <Text>Update User Info...</Text>                
-                     </View>
-                   );
-                 }
-               }
+  render() {
+    const { menuItems } = this.state;
+    return (
+      <View>
+        {menuItems.map((item, index) => (
+          <ListItem
+            key={index}
+            title={item.title}
+            leftIcon={{
+              type: item.iconType,
+              name: item.iconNameLeft,
+              color: item.iconColorLeft
+            }}
+            rightIcon={{
+              type: item.iconType,
+              name: item.iconNameRight,
+              color: item.iconColorRight
+            }}
+            onPress={item.onPress}
+            containerStyle={styles.contentContainerStyle}
+          />
+        ))}
+      </View>
+    );
+  }
+}
 
-const styles = StyleSheet.create({});
-    
+const styles = StyleSheet.create({
+  contentContainerStyle: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#e3e3e3"
+  }
+});
